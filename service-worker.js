7e3c634dbcb6058386695756e95c361669d7b310
cataldosabato <cataldosabato04@gmail.com>
2026-09-15 15:24:12 +0200

    primo commit

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'magazzino-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+// Installazione del Service Worker e salvataggio dei file in cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercettazione delle richieste di rete (per funzionare offline)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Ritorna la risorsa dalla cache se esiste, altrimenti la scarica
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Pulizia delle vecchie cache durante l'aggiornamento
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
